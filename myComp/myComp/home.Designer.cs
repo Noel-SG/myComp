6497a11e5740456383e2b91361e5c64e6a4f62be
Noel-SG <45712638+Noel-SG@users.noreply.github.com>
2019-03-25 11:19:26 +0100

    General info tab

--- myComp/myComp/home.Designer.cs
+++ myComp/myComp/home.Designer.cs
@@ -24,7 +24,29 @@
         /// </summary>
         private void InitializeComponent() {
             this.btnAbout = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grpGeneralInformation = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFLDUsrLogged = new System.Windows.Forms.TextBox();
+            this.txtFLDOSPlatform = new System.Windows.Forms.TextBox();
+            this.txtFLDOSVersion = new System.Windows.Forms.TextBox();
+            this.txtFLDOS = new System.Windows.Forms.TextBox();
+            this.txtFLDDeviceName = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetAllDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grpGeneralInformation.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAbout
@@ -39,34 +61,265 @@
             this.btnAbout.Text = "About";
             this.btnAbout.UseVisualStyleBackColor = false;
             // 
-            // groupBox1
+            // grpGeneralInformation
             // 
-            this.groupBox1.Location = new System.Drawing.Point(12, 58);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(295, 262);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "General information";
+            this.grpGeneralInformation.Controls.Add(this.label5);
+            this.grpGeneralInformation.Controls.Add(this.label4);
+            this.grpGeneralInformation.Controls.Add(this.txtFLDUsrLogged);
+            this.grpGeneralInformation.Controls.Add(this.txtFLDOSPlatform);
+            this.grpGeneralInformation.Controls.Add(this.txtFLDOSVersion);
+            this.grpGeneralInformation.Controls.Add(this.txtFLDOS);
+            this.grpGeneralInformation.Controls.Add(this.txtFLDDeviceName);
+            this.grpGeneralInformation.Controls.Add(this.textBox3);
+            this.grpGeneralInformation.Controls.Add(this.textBox2);
+            this.grpGeneralInformation.Controls.Add(this.label3);
+            this.grpGeneralInformation.Controls.Add(this.label2);
+            this.grpGeneralInformation.Controls.Add(this.label1);
+            this.grpGeneralInformation.Location = new System.Drawing.Point(12, 35);
+            this.grpGeneralInformation.Name = "grpGeneralInformation";
+            this.grpGeneralInformation.Size = new System.Drawing.Size(466, 153);
+            this.grpGeneralInformation.TabIndex = 1;
+            this.grpGeneralInformation.TabStop = false;
+            this.grpGeneralInformation.Text = "General information";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 14);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Operating system platform:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 14);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Current user logged in:";
+            // 
+            // txtFLDUsrLogged
+            // 
+            this.txtFLDUsrLogged.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtFLDUsrLogged.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFLDUsrLogged.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFLDUsrLogged.Location = new System.Drawing.Point(176, 124);
+            this.txtFLDUsrLogged.Name = "txtFLDUsrLogged";
+            this.txtFLDUsrLogged.ShortcutsEnabled = false;
+            this.txtFLDUsrLogged.Size = new System.Drawing.Size(274, 17);
+            this.txtFLDUsrLogged.TabIndex = 10;
+            // 
+            // txtFLDOSPlatform
+            // 
+            this.txtFLDOSPlatform.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtFLDOSPlatform.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFLDOSPlatform.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFLDOSPlatform.Location = new System.Drawing.Point(176, 100);
+            this.txtFLDOSPlatform.Name = "txtFLDOSPlatform";
+            this.txtFLDOSPlatform.ShortcutsEnabled = false;
+            this.txtFLDOSPlatform.Size = new System.Drawing.Size(274, 17);
+            this.txtFLDOSPlatform.TabIndex = 9;
+            // 
+            // txtFLDOSVersion
+            // 
+            this.txtFLDOSVersion.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtFLDOSVersion.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFLDOSVersion.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFLDOSVersion.Location = new System.Drawing.Point(176, 76);
+            this.txtFLDOSVersion.Name = "txtFLDOSVersion";
+            this.txtFLDOSVersion.ShortcutsEnabled = false;
+            this.txtFLDOSVersion.Size = new System.Drawing.Size(274, 17);
+            this.txtFLDOSVersion.TabIndex = 8;
+            // 
+            // txtFLDOS
+            // 
+            this.txtFLDOS.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtFLDOS.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFLDOS.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFLDOS.Location = new System.Drawing.Point(176, 52);
+            this.txtFLDOS.Name = "txtFLDOS";
+            this.txtFLDOS.ShortcutsEnabled = false;
+            this.txtFLDOS.Size = new System.Drawing.Size(274, 17);
+            this.txtFLDOS.TabIndex = 7;
+            // 
+            // txtFLDDeviceName
+            // 
+            this.txtFLDDeviceName.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtFLDDeviceName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFLDDeviceName.Font = new System.Drawing.Font("Microsoft Tai Le", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFLDDeviceName.Location = new System.Drawing.Point(176, 28);
+            this.txtFLDDeviceName.Name = "txtFLDDeviceName";
+            this.txtFLDDeviceName.ShortcutsEnabled = false;
+            this.txtFLDDeviceName.Size = new System.Drawing.Size(274, 17);
+            this.txtFLDDeviceName.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(168, 80);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(274, 14);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(168, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(274, 14);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Operating system version:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operating system:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Device name:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.SeaGreen;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(899, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetAllDataToolStripMenuItem,
+            this.toolStripSeparator1});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // resetAllDataToolStripMenuItem
+            // 
+            this.resetAllDataToolStripMenuItem.Name = "resetAllDataToolStripMenuItem";
+            this.resetAllDataToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.resetAllDataToolStripMenuItem.Text = "Reset all data";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(145, 6);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F);
+            this.viewToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F);
+            this.helpToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Kozuka Gothic Pro R", 9F);
+            this.aboutToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(812, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Print ";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 485);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(899, 485);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grpGeneralInformation);
             this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "myComp";
+            this.Load += new System.EventHandler(this.home_Load);
+            this.grpGeneralInformation.ResumeLayout(false);
+            this.grpGeneralInformation.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnAbout;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grpGeneralInformation;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetAllDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtFLDOSVersion;
+        private System.Windows.Forms.TextBox txtFLDOS;
+        private System.Windows.Forms.TextBox txtFLDDeviceName;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFLDUsrLogged;
+        private System.Windows.Forms.TextBox txtFLDOSPlatform;
     }
 }
--- myComp/myComp/home.cs
+++ myComp/myComp/home.cs
@@ -13,5 +13,23 @@ namespace myComp {
         public home() {
             InitializeComponent();
         }
+
+        private void home_Load(object sender, EventArgs e) {
+            String q1 = Environment.UserName;
+            txtFLDUsrLogged.Text = Convert.ToString(q1);
+
+            String q2 = Environment.MachineName;
+            txtFLDDeviceName.Text = Convert.ToString(q2);
+
+            String q8 = (Environment.OSVersion.ToString());
+            txtFLDOS.Text = q8;
+
+            String q9 = Environment.OSVersion.Platform.ToString();
+            txtFLDOSPlatform.Text = q9;
+
+            String q10 = Environment.OSVersion.Version.ToString();
+            txtFLDOSVersion.Text = q10;
+
+        }
     }
 }
